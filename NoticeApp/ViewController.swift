4098ee27af3baba5c5c048d58b4d2ab83be75e5a
zino <minzino@hanmail.net>
2020-03-13 14:33:57 +0900

    Initial Commit

---
+++ NoticeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoticeApp
+//
+//  Created by zino-mac on 2020/03/13.
+//  Copyright © 2020 zino-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
